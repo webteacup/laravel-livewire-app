a9a1fe98776b85165bbfed9dbb55fd97cc992dec
webteacup <supermariodev1220@gmail.com>
2026-04-27 22:45:21 -0400

    update example test php

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,13 +2,12 @@
 
 namespace Tests\Feature;
 
-use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
     /**
-     * A basic test example.
+     * Guests are redirected to the sign-in page from root.
      *
      * @return void
      */
@@ -16,6 +15,6 @@ class ExampleTest extends TestCase
     {
         $response = $this->get('/');
 
-        $response->assertStatus(200);
+        $response->assertRedirect(route('login'));
     }
 }
